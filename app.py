db449852b404603e3489bce8ce47926f8dcd3f46
Ariel Spencer <arielspencer.tech@gmail.com>
2025-02-18 21:44:58 -0300

    feat: add menu with logo

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('Hello, World!')
+print('*************')
+print('⟆Ꭿᑲ𝖮ᖇ ᕮⲭᕈᖇ∈⟆⟆')
+print('*************')
+
+print('\n1. Cadastrar Restaurante')
+print('2. Listar Restaurantes')
+print('3. Ativar Restaurante')
+print('4. Sair')
+
+chosen_option = input('\nEscolha uma opção: ')
+print(f'Opção escolhida: {chosen_option}')
